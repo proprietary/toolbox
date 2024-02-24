8418548bf4443b17f93ac28e0c99de9456aa6a46
Zelly Snyder <zelcon@zelcon.net>
2024-02-24 08:59:54 -0800

    update build script for darwin emacs
    This allows for building two different binaries—one NS app that you
    can put in your dock and another for the terminal and installed in
    /usr/local

--- src/build_emacs_darwin/build_emacs_darwin.sh
+++ src/build_emacs_darwin/build_emacs_darwin.sh
@@ -21,7 +21,7 @@
 #        gnutls \
 #        tree-sitter
 
-GIT_REF="emacs-29"
+GIT_REF="master"
 PREFIX="/usr/local"
 CFLAGS="-O3 -march=native -I/opt/local/include/gcc13"
 LDFLAGS="-L/opt/local/lib/gcc13 \
@@ -29,32 +29,77 @@ LDFLAGS="-L/opt/local/lib/gcc13 \
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -e
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 [--with-ns|--without-ns]"
+    exit 1
+fi
+
+function parse_arguments() {
+    WITH_NS=false
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case "$key" in
+            --with-ns)
+                WITH_NS=true
+                shift
+                ;;
+            --without-ns)
+                WITH_NS=false
+                shift
+                ;;
+            *)
+                echo "Unknown argument: $key"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+parse_arguments "$@"
+echo "Building as a NS app: $WITH_NS…"
+
 if [ ! -d "$THIS_DIR/emacs" ]; then
     git clone git://git.savannah.gnu.org/emacs.git
 fi
 
 cd "$THIS_DIR/emacs"
 
-git checkout "${GIT_REF}" && git pull
+git clean -fxd && \
+  git reset --hard && \
+  git checkout "${GIT_REF}" && \
+  git pull
 
 ./autogen.sh
 
-./configure \
-    --prefix="${PREFIX}" \
-    --with-ns \
-    --disable-ns-self-contained \
-    --with-json \
-    --with-tree-sitter \
-    --with-native-compilation=aot \
-    --with-imagemagick \
-    --with-mailutils \
-    CFLAGS="${CFLAGS}" \
-    LDFLAGS="${LDFLAGS}"
+if [[ "${WITH_NS}" == "false" ]]; then
+    ./configure \
+        --with-ns \
+        --with-json \
+        --with-tree-sitter \
+        --with-native-compilation=aot \
+        --with-imagemagick \
+        --with-mailutils \
+        CFLAGS="${CFLAGS}" \
+        LDFLAGS="${LDFLAGS}"
 
-if [ -ne $? 0 ]; then
-  exit 1
-fi
-
-gmake -j $(sysctl -n hw.ncpu) && \
-    sudo gmake install && \
+    gmake -j $(sysctl -n hw.ncpu) && \
+    gmake install && \
     cp -r nextstep/Emacs.app /Applications/
+else
+    # Build non-NS version for the terminal
+    ./configure \
+        --prefix="${PREFIX}" \
+        --without-ns \
+        --with-json \
+        --with-tree-sitter \
+        --with-native-compilation=aot \
+        --with-imagemagick \
+        --with-mailutils \
+        CFLAGS="${CFLAGS}" \
+        LDFLAGS="${LDFLAGS}"
+
+    gmake -j $(sysctl -n hw.ncpu) && \
+        sudo gmake install
+fi
